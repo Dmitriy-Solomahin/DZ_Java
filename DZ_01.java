48a194f1d2a5083ac74cdb22c791670d0e9705d6
Dmitriy Solomahin <solomahin_dima@mail.ru>
2023-03-04 16:10:56 +0300

    debugging dz1

--- DZ_01.java
+++ DZ_01.java
@@ -4,17 +4,20 @@ import java.util.Scanner;
 
 public class DZ_01 {
     public static void main(String[] args) {
-        task1();
-        System.out.println(task2());
-        task3();
+        Scanner inp = new Scanner(System.in);
+        task1(inp);
+        System.out.println(task2(inp));
+        task3(inp);
+        inp.close();
+
     }
 
-    private static void task1() {
+    private static void task1(Scanner inp) {
         // Задать одномерный массив и найти в нем минимальный и максимальный элементы
-        Scanner inp = new Scanner(System.in);
+        // Scanner inp = new Scanner(System.in);
         System.out.print("Введите длину масива: ");
         int size = inp.nextInt();
-        inp.close();
+        // inp.close();
         int[] intArray = new int[size];
         for (int i = 0; i < size; i++) {
             double a = Math.random();
@@ -35,14 +38,16 @@ public class DZ_01 {
 
     }
 
-    private static boolean task2() {
-        // Написать метод, который определяет, является ли год високосным, и возвращает boolean
-        // (високосный - true, не високосный - false). 
-        // Каждый 4-й год является високосным, кроме каждого 100-го, при этом каждый 400-й – високосный.
-        Scanner inp = new Scanner(System.in);
+    private static boolean task2(Scanner inp) {
+        // Написать метод, который определяет, является ли год високосным, и возвращает
+        // boolean
+        // (високосный - true, не високосный - false).
+        // Каждый 4-й год является високосным, кроме каждого 100-го, при этом каждый
+        // 400-й – високосный.
+        // Scanner inp = new Scanner(System.in);
         System.out.println("Введите год для проверки: ");
         int year = inp.nextInt();
-        inp.close();
+        // inp.close();
         boolean result = false;
         if (year % 4 == 0) {
             if ((year % 100 == 0) && (year % 400 != 0)) {
@@ -54,29 +59,27 @@ public class DZ_01 {
         return result;
     }
 
-    private static void task3() {
+    private static void task3(Scanner inp) {
         // Дан массив nums = [3,2,2,3] и число val = 3.
         // Если в массиве есть числа, равные заданному, нужно перенести эти элементы в
         // конец массива.
         // Таким образом, первые несколько (или все) элементов массива должны быть
         // отличны от заданного, а остальные - равны ему.
-        int[] intArray = {3,2,2,3,3,2,3,2,2,3,2};
-        Scanner inp = new Scanner(System.in);
+        int[] intArray = { 3, 2, 2, 3, 3, 2, 3, 2, 2, 3, 2 };
+        // Scanner inp = new Scanner(System.in);
         System.out.print("Введите число для сортировки: ");
         int num = inp.nextInt();
-        inp.close();
+        // inp.close();
         int i = 0;
-        int j = intArray.length-1;
+        int j = intArray.length - 1;
         while (i < j) {
-            if (intArray[j] == num){
+            if (intArray[j] == num) {
                 j--;
-            }
-            else if (intArray[i] == num){
+            } else if (intArray[i] == num) {
                 intArray[i] = intArray[j];
                 intArray[j] = num;
                 i++;
-            }
-            else {
+            } else {
                 i++;
             }
         }
